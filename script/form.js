eea35c39e0f115030fe1fccec3fccfff487b8199
Nisanthan K <Nisanthan@email.com>
2022-09-09 22:47:58 +0200

    Added: New Changes to check contact

--- script/form.js
+++ script/form.js
@@ -38,17 +38,17 @@ function takeFileFromJson() {
             //showDataForTask(response);
             let data = JSON.parse(response)
             for (const field of data.contact.standard) {
-                document.getElementById("formFile").innerHTML += field["field_name"]
-                document.getElementById("formFile").innerHTML += field["marketing-in-one"]
-                document.getElementById("formFile").innerHTML += field["email"]
-                document.getElementById("formFile").innerHTML += field["datatype"]
-                document.getElementById("formFile").innerHTML += field["required"]
-                document.getElementById("formFile").innerHTML += field["html_label"]
-                document.getElementById("formFile").innerHTML += field["html_placeholder"]
-                document.getElementById("formFile").innerHTML += field["html_hidden_field"]
-                document.getElementById("formFile").innerHTML += field["field_type"]
-                document.getElementById("formFile").innerHTML += field["php_type"]
-                document.getElementById("formFile").innerHTML += field["regex"]
+                document.getElementById("formFile").innerHTML += field["field_name"] + "</br>"
+                document.getElementById("formFile").innerHTML += field["marketing-in-one"] + "</br>"
+                document.getElementById("formFile").innerHTML += field["email"] + "</br>"
+                document.getElementById("formFile").innerHTML += field["datatype"] + "</br>"
+                document.getElementById("formFile").innerHTML += field["required"] + "</br>"
+                document.getElementById("formFile").innerHTML += field["html_label"] + "</br>"
+                document.getElementById("formFile").innerHTML += field["html_placeholder"] + "</br>"
+                document.getElementById("formFile").innerHTML += field["html_hidden_field"] + "</br>"
+                document.getElementById("formFile").innerHTML += field["field_type"] + "</br>"
+                document.getElementById("formFile").innerHTML += field["php_type"] + "</br>"
+                document.getElementById("formFile").innerHTML += field["regex"] + "</br>"
                 document.getElementById("formFile").innerHTML += "</br></br>"
                 
                 // document.getElementById("formFile").innerHTML += `<input type="${field.datatype}"> </br></br>`;
