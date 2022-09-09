1b27a10d2330c35471928ed379a11ce8f36a4e28
Nisanthan K <Nisanthan@email.com>
2022-09-09 22:46:56 +0200

    Added: New Changes to check contact

--- script/form.js
+++ script/form.js
@@ -38,7 +38,20 @@ function takeFileFromJson() {
             //showDataForTask(response);
             let data = JSON.parse(response)
             for (const field of data.contact.standard) {
-                document.getElementById("formFile").innerHTML += `<input type="${field.datatype}"> </br></br>`;
+                document.getElementById("formFile").innerHTML += field["field_name"]
+                document.getElementById("formFile").innerHTML += field["marketing-in-one"]
+                document.getElementById("formFile").innerHTML += field["email"]
+                document.getElementById("formFile").innerHTML += field["datatype"]
+                document.getElementById("formFile").innerHTML += field["required"]
+                document.getElementById("formFile").innerHTML += field["html_label"]
+                document.getElementById("formFile").innerHTML += field["html_placeholder"]
+                document.getElementById("formFile").innerHTML += field["html_hidden_field"]
+                document.getElementById("formFile").innerHTML += field["field_type"]
+                document.getElementById("formFile").innerHTML += field["php_type"]
+                document.getElementById("formFile").innerHTML += field["regex"]
+                document.getElementById("formFile").innerHTML += "</br></br>"
+                
+                // document.getElementById("formFile").innerHTML += `<input type="${field.datatype}"> </br></br>`;
             }
         }
     });
